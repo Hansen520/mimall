688b4a43b0afb0549097c6c03b97a60d3ac25c24
dream1994 <646380243@qq.com>
2020-01-19 23:29:22 +0800

    目录解构

---
+++ src/pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-fotter></nav-fotter>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+
+</script>
+
+<style>
+
+</style>
